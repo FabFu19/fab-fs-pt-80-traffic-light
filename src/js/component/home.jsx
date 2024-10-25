36f2096e62a609b132778e4e4b2a9e44252a1f5f
FabFu19 <fabianfuen19@icloud.com>
2024-10-25 19:40:49 +0000

    feat: select color traffic light created

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,25 +1,20 @@
-import React from "react";
+import React, {useState} from "react";
 
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+//import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	const [light, setlight] = useState('') ;
+	
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+		<>
+			<div className="container bg-dark">
+				<div onClick={()=>setlight('red')} className={`first-color-light ${light === 'red' ? 'traffic-shadow' : ''} my-3`}></div>
+				<div onClick={()=>setlight('yellow')} className={`second-color-light ${light === 'yellow' ? 'traffic-shadow' : ''} mb-3`}></div>
+				<div onClick={()=>setlight('green')} className={`third-color-light ${light === 'green' ? 'traffic-shadow' : ''} mb-3`}></div>
+			</div>
+		</>
 	);
 };
 
