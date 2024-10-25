ed0bdaaca4a3576e682d25e22c08601f8685c60f
FabFu19 <fabianfuen19@icloud.com>
2024-10-25 20:10:01 +0000

    feat(button added): toggle crated

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -6,13 +6,16 @@ import React, {useState} from "react";
 //create your first component
 const Home = () => {
 	const [light, setlight] = useState('') ;
-	
+	const [showLight, setShowLight] = useState(false);
+	const getToggle = ()=>setShowLight(prev => prev=!prev);
 	return (
 		<>
 			<div className="container bg-dark">
-				<div onClick={()=>setlight('red')} className={`first-color-light ${light === 'red' ? 'traffic-shadow' : ''} my-3`}></div>
-				<div onClick={()=>setlight('yellow')} className={`second-color-light ${light === 'yellow' ? 'traffic-shadow' : ''} mb-3`}></div>
-				<div onClick={()=>setlight('green')} className={`third-color-light ${light === 'green' ? 'traffic-shadow' : ''} mb-3`}></div>
+				<div onClick={()=>setlight(prev => prev='red')} className={`first-color-light ${light === 'red' ? 'traffic-shadow' : ''} my-3`}></div>
+				<div onClick={()=>setlight(prev => prev='yellow')} className={`second-color-light ${light === 'yellow' ? 'traffic-shadow' : ''} mb-3`}></div>
+				<div onClick={()=>setlight(prev => prev='green')} className={`third-color-light ${light === 'green' ? 'traffic-shadow' : ''} mb-3`}></div>
+				{showLight && <div onClick={()=>setlight(prev => prev='purple')} className={`fourth-color-light ${light === 'purple' ? 'traffic-shadow' : ''} mb-3`}></div>}
+				<div onClick={getToggle} className="btn btn-outline-light my-3">Add Light</div>
 			</div>
 		</>
 	);
